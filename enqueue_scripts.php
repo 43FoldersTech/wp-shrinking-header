8edeeccc044caf9309ec5c00ae1eb5bf82b22687
Michael Kastler <michael@43folderstech.net>
2016-05-02 14:10:57 -0500

    Remove JS References
    Better to use the php file, which gets loaded automatically.  This also
    reduces enqueues.

--- enqueue_scripts.php
+++ enqueue_scripts.php
@@ -5,8 +5,6 @@ function enqueue_43folders_scripts() {
 
     wp_register_style( 'customstyling', CHILD_THEME_URL . '/43folders/customstyling.css', array(), CHILD_THEME_VERSION );
     wp_enqueue_style( 'customstyling' );
-	wp_register_script( 'customscripts', CHILD_THEME_URL . '/43folders/customscripts.js', array( 'jquery' ), '1.0.0' );    
-	wp_enqueue_script( 'customscripts' );   
     
     //* add new action to allow for shrinking header
     add_action ( 'wp_footer', 'gen_shrink_head', 1 );
